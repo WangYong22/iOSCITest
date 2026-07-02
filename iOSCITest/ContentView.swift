e9593a61f33cfc4251bb1171633e93d2f09006a9
wangyong <wangyong2@gmail.com>
2026-07-02 23:18:11 +0800

    change to 10

--- iOSCITest/ContentView.swift
+++ iOSCITest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, 9 world!")
+            Text("Hello, 10 world!")
         }
         .padding()
     }
